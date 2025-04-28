2c0f898f827d0912bd6766a3fb15e67600a7fbee
Eric <ericnie12310@gmail.com>
2025-04-28 11:15:19 -0400

    ..

--- src/components/utils/fetchData.js
+++ src/components/utils/fetchData.js
@@ -1,56 +1,37 @@
 import axios from "axios";
 
 export const fetchData = async (id) => {
-
   let responseArr = [];
-  const baseApiUrl = "https://www.googleapis.com/youtube/v3";
-  const apikey = "AIzaSyA26jr5qEuHbevwqLJhS7BS2ywIz2jZSTs";
+  const baseApiUrl = "http://localhost:5000/api";
 
-  const playlistDetails = await axios.get(`${baseApiUrl}/playlists`, {
-    params: {
-      part: "snippet",
-      id: id,
-      key: apikey,
-    },
-  });
-  const playlistDetailsObject = {
-    playlistName: playlistDetails.data.items[0].snippet.title,
-    playlistId: playlistDetails.data.items[0].id,
-    PlaylistImage: playlistDetails.data.items[0].snippet.thumbnails.medium.url,
-  };
   try {
-    let nextToken = "";
+    const response = await axios.get(`${baseApiUrl}/playlist`, {
+      params: { playlistId: id },
+    });
 
-    // addToPlaylistDetails(playlistDetailsObject)
+    const playlistDetails = response.data.playlistDetails;
+    const playlistItems = response.data.playlistItems;
 
-    do {
-      const responseListItems = await axios.get(`${baseApiUrl}/playlistItems`, {
-        params: {
-          part: "snippet",
-          maxResults: 50,
-          key: apikey,
-          pageToken: nextToken,
-          playlistId: id,
-        },
-      });
-      console.log("AXIOS RESPONSE", responseListItems);
-      responseArr.push(...responseListItems.data.items);
-      if (responseListItems.data.nextPageToken) {
-        nextToken = responseListItems.data.nextPageToken;
-      } else nextToken = "";
-    } while (nextToken);
-    // addSongs(responseArr);
-    // currentSong(responseArr[0].snippet.resourceId.videoId);
-    // nextSong(responseArr[1].snippet.resourceId.videoId);
+    // Map playlist details
+    const playlistDetailsObject = {
+      playlistName: playlistDetails.snippet.title,
+      playlistId: playlistDetails.id,
+      PlaylistImage: playlistDetails.snippet.thumbnails.medium.url,
+    };
+
+    // Map playlist items
+    responseArr = playlistItems;
+
+    const dataReturned = {
+      playlistDetailsObject: playlistDetailsObject,
+      responseArrToAdd: responseArr,
+      currentSong: responseArr[0].snippet.resourceId.videoId,
+      nextSong: responseArr[1].snippet.resourceId.videoId,
+    };
+
+    return dataReturned;
   } catch (error) {
-    console.error(error);
+    console.error("Error fetching data from backend:", error);
+    throw error;
   }
-  const dataReturned = {
-    playlistDetailsObject: playlistDetailsObject,
-    responseArrToAdd: responseArr,
-    currentSong: responseArr[0].snippet.resourceId.videoId,
-    nextSong: responseArr[1].snippet.resourceId.videoId,
-  };
-
-  return dataReturned;
 };
