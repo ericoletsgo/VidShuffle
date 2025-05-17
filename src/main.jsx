bb3c586cbd0f9511fc27a59c1a5fa24c3e916fd0
Eric <ericnie12310@gmail.com>
2025-05-17 14:22:00 -0400

    enable redux persist to save session

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 import { BrowserRouter } from "react-router-dom";
-import store from "./store";
+import { store, persistor } from "./store";
 
 const basename = import.meta.env.DEV ? '/VidShuffle/' : '/';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter basename={basename}>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </BrowserRouter>
 );
