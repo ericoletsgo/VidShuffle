423d436cb824213294de313d2dcf041a03a3c292
Eric <ericnie12310@gmail.com>
2025-06-06 19:30:00 -0400

    agent api client for frontend

---
+++ src/components/utils/agentApi.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+const AGENT_URL = import.meta.env.VITE_AGENT_URL || "http://localhost:8000";
+
+export const getVideoInsight = async (videoId, title) => {
+  const res = await axios.post(`${AGENT_URL}/insight/video`, {
+    video_id: videoId,
+    title,
+  });
+  return res.data;
+};
+
+export const getPlaylistInsight = async (videos) => {
+  const res = await axios.post(`${AGENT_URL}/insight/playlist`, { videos });
+  return res.data;
+};
+
+export const searchPlaylist = async (query, videoIds) => {
+  const res = await axios.post(`${AGENT_URL}/search`, {
+    query,
+    video_ids: videoIds,
+  });
+  return res.data.results;
+};
