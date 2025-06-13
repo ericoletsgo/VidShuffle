732f89e5e786ec8635c353c19d4bdd4ff8d8094c
Eric <ericnie12310@gmail.com>
2025-06-13 19:15:00 -0400

    simplify frontend api client

--- src/components/utils/agentApi.js
+++ src/components/utils/agentApi.js
@@ -2,25 +2,8 @@ import axios from "axios";
 
 const BASE_URL = "https://vid-shuffle.vercel.app/api";
 
-export const checkAgentHealth = async () => {
-  try {
-    const res = await axios.get(`${BASE_URL}/health`, { timeout: 3000 });
-    return res.data.status === "ok";
-  } catch {
-    return false;
-  }
-};
-
-export const getVideoInsight = async (videoId, title) => {
-  const res = await axios.post(`${BASE_URL}/insight/video`, {
-    video_id: videoId,
-    title,
-  });
-  return res.data;
-};
-
-export const getPlaylistInsight = async (videos) => {
-  const res = await axios.post(`${BASE_URL}/insight/playlist`, { videos });
+export const analyzePlaylist = async (videos) => {
+  const res = await axios.post(`${BASE_URL}/analyze`, { videos }, { timeout: 60000 });
   return res.data;
 };
 
@@ -28,6 +11,6 @@ export const searchPlaylist = async (query, videoIds) => {
   const res = await axios.post(`${BASE_URL}/search`, {
     query,
     video_ids: videoIds,
-  });
+  }, { timeout: 30000 });
   return res.data.results;
 };
