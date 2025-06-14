928d5351002db9c023fe08518b98ec2a66ce060b
Eric <ericnie12310@gmail.com>
2025-06-14 11:00:00 -0400

    fix regex pattern for newer browsers

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -46,7 +46,7 @@ const Search = ({ addSongs, currentSong, nextSong, addToPlaylistDetails }) => {
       <form onSubmit={(e) => handleSubmit(e)}>
         <input
         className="inputSearch"
-          pattern="^(https?:\/\/)?(www\.)?youtube\.com\/playlist\?list=PL[a-zA-Z0-9_-]+$|^(PL[a-zA-Z0-9_-]+)$"
+          pattern="^(https?://)?(www\.)?youtube\.com/playlist\?list=PL[a-zA-Z0-9_\-]+$|^(PL[a-zA-Z0-9_\-]+)$"
           title="Please enter a valid YouTube playlist URL or ID"
           type="text"
           autoFocus
